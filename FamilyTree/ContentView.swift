0d37118194d4cb7ec88aa1b8be13ae935645e7b2
bayo <bayodeog@gmail.com>
2026-01-22 00:06:30 +0100

    First family tree built here

--- FamilyTree/ContentView.swift
+++ FamilyTree/ContentView.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("A family tree")
+            
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.blue)
+            
+            Text("A family tree is a diagram that shows the \nrelationships of people, typically \narranged by generation.")
+            
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .fontWeight(.regular)
+                .foregroundStyle(.blue)
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 800, height: 400)
+                .clipShape(.buttonBorder)
+            
+            Text(message)
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundStyle(.blue)
+                .fontWidth(.standard)
+            
+            Spacer()
         }
+       
+        HStack {
+            Button("Father") {
+                message = "I am the Father"
+                imageName = "Father"
+            }
+            Button("Mother") {
+                message = "I am the Mother"
+                imageName = "Mother"
+            }
+            Button("Children") {
+                message = "We are the Children"
+                imageName = "Children"
+            }
+        }
+        
+        .foregroundStyle(.white)
+        .tint(.blue)
+        .buttonBorderShape(.roundedRectangle)
+        .buttonStyle(.borderedProminent)
+        .buttonSizing(.flexible)
+        
         .padding()
     }
 }
