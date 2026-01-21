ae5f05200318f7ba9228bfaef7c08dfa111f31f3
bayo <bayodeog@gmail.com>
2026-01-21 21:57:40 +0100

    Initial Commit

---
+++ FamilyTree/FamilyTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyTreeApp.swift
+//  FamilyTree
+//
+//  Created by Adebayode Adegoke on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FamilyTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
